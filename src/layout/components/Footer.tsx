d071721a154d3079754f7ec22829596ffbcf3b91
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-05-05 23:39:48 +0600

    header & footer

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,3 +1,32 @@
+import { FaFacebook } from '@react-icons/all-files/fa/FaFacebook';
+import { FaGithub } from '@react-icons/all-files/fa/FaGithub';
+import { FaInstagram } from '@react-icons/all-files/fa/FaInstagram';
+import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter';
+import { SiGitlab } from '@react-icons/all-files/si/SiGitlab';
+
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer className='footer items-center border-t border-gray-800 p-5 text-neutral-content'>
+      <div className='grid-flow-col items-center'>
+        <h1 className='text-2xl'>Mugdha Arunim Ahmed</h1>
+      </div>
+      <div className='grid-flow-col gap-4 text-base md:place-self-center md:justify-self-end'>
+        <a>
+          <FaFacebook className='h-6 w-6 fill-current' />
+        </a>
+        <a>
+          <FaInstagram className='h-6 w-6 fill-current' />
+        </a>
+        <a>
+          <FaTwitter className='h-6 w-6 fill-current' />
+        </a>
+        <a>
+          <FaGithub className='h-6 w-6 fill-current' />
+        </a>
+        <a>
+          <SiGitlab className='h-6 w-6 fill-current' />
+        </a>
+      </div>
+    </footer>
+  );
 }
--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <nav className='navbar sticky top-0 z-10 bg-green-400 bg-opacity-30 px-20 backdrop-blur-lg backdrop-filter'>
+      <Link href='/'>
+        <a className='text-xl capitalize'>Mugdha Arunim Ahmed</a>
+      </Link>
+      <ul className='menu menu-horizontal ml-auto p-0'>
+        <li>
+          <Link href='/blog'>Blog</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
