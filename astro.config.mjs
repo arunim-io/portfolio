b05e8f369c7b7462fa96a4e835cbf28582400090
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-07-13 12:41:17 +0600

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()],
   vite: {
     ssr: {
       external: ['svgo'],
