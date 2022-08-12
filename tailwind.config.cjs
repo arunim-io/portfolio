8b5449aedeedf7c89f2ca722a44a017bd514497e
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-08-13 01:10:13 +0600

    Greeting on Home Page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,23 @@ const config = {
   plugins: [require('daisyui')],
   theme: {
     extend: {
+      animation: {
+        typing: '4s typing-erase 4s steps(50, end) infinite, blink-caret .5s step-end infinite',
+      },
+      keyframes: {
+        'blink-caret': {
+          'from, to': { borderColor: 'transparent' },
+          '50%': { borderColor: '#1eb854' },
+        },
+        'typing-erase': {
+          '0%': { width: '0' },
+          '80%': { width: '100%' },
+          '90%, 100%': { width: '0' },
+        },
+      },
+      maxWidth: {
+        content: 'max-content',
+      },
       zIndex: {
         max: '9999999999',
       },
