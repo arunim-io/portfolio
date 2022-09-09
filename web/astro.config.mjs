934a3efc2e1b31f4d4cf2eb08f08c8724b48d65b
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-09-09 20:25:38 +0600

    add intro

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,12 @@
+import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), image()],
   vite: {
     ssr: {
       external: ['svgo', 'graphql'],
