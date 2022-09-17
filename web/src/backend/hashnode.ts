93673157b2f2f76affa9642dec843e48fa344947
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-09-17 23:42:32 +0600

    removing sanity and pnpm workspace

--- web/src/backend/hashnode.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const hashnodeCLient = new ApolloClient({
-  uri: 'https://api.hashnode.com',
-  cache: new InMemoryCache(),
-});
-
-export default hashnodeCLient;
