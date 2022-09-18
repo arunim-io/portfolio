d63716483435e7b55b50af6ac6646ba42e9c3eb2
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-09-18 22:48:03 +0600

    add compressing support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), image()],
+  integrations: [image(), sitemap(), tailwind(), compress()],
+  site: 'https://arunim-portfolio.vercel.app/',
   vite: {
     ssr: {
-      external: ['svgo', 'graphql'],
+      external: ['svgo'],
     },
   },
 });
