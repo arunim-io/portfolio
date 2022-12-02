26285b706ced16956b11732eb87e5befe00f86cb
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-12-03 00:03:27 +0600

    fixes

--- src/layouts/Main.svelte
+++ src/layouts/Main.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { AppShell } from '@brainandbones/skeleton';
 
   import Header from '~/components/Header.svelte';
@@ -8,7 +8,5 @@
 <AppShell slotSidebarRight="hidden md:block">
   <Header slot="header" />
   <Sidebar slot="sidebarRight" />
-  <main class="flex flex-col gap-y-20 my-20 mx-10 sm:mx-16 lg:mx-20">
-    <slot />
-  </main>
+  <slot />
 </AppShell>
