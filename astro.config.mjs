9a2ef80d3a0a0bcb0125cbf25afd30499f71987c
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-12-04 18:27:54 +0600

    fix build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,12 @@
-import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
-import AutoImport from 'unplugin-auto-import/astro';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-  integrations: [
-    AutoImport({
-      dts: true,
-      eslintrc: {
-        enabled: true,
-      },
-      imports: ['svelte'],
-    }),
-    image(),
-    sitemap(),
-    tailwind(),
-    compress(),
-    svelte(),
-  ],
+  integrations: [sitemap(), svelte(), tailwind(), compress()],
   site: 'https://arunim-portfolio.vercel.app/',
   vite: {
     plugins: [
