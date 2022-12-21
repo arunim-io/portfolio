76ee8e7a2b1512447755150ac0325d421ef2c69b
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-12-21 20:16:01 +0600

    fix icon issues

--- src/components/TechList.svelte
+++ src/components/TechList.svelte
@@ -1,14 +1,16 @@
 <script>
   import { skills } from '~/constants';
+
+  import Icon from './Icon.svelte';
 </script>
 
 <ul
   class="logo-cloud grid-cols-1 gap-2.5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
 >
-  {#each skills as { icon: Icon, name }}
+  {#each skills as { icon, name }}
     <li class="logo-item p-20">
       <span>
-        <Icon class="h-10 w-10" />
+        <Icon {icon} class="h-10 w-10" />
       </span>
       <span>{name}</span>
     </li>
